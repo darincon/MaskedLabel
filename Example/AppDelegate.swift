7132ba27f3e074da5555946de32b60aecf82e81b
Diego Rincon <drincon@scirestudios.com>
2017-04-17 17:40:59 -0500

    Project setup

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GradientTextLabel
+//
+//  Created by Diego Rincon on 4/17/17.
+//  Copyright © 2017 Scire Studios. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
